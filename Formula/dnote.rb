2ec29bf7abcb9279791ad0525739c94d7ada5808
sungwoncho <mikeswcho@gmail.com>
2018-09-08 16:24:38 +1000

    Brew formula update for cli version v0.4.3

--- Formula/dnote.rb
+++ Formula/dnote.rb
@@ -1,9 +1,9 @@
 class Dnote < Formula
   desc "Capture your learning without leaving the command line"
   homepage "https://dnote.io"
-  url "https://github.com/dnote/cli/releases/download/v0.4.2/dnote_0.4.2_darwin_amd64.tar.gz"
-  version "0.4.2"
-  sha256 "d78871314d52dc48b18200fff896de63db55077ef2ce4e5f407ab92a72873c00"
+  url "https://github.com/dnote/cli/releases/download/v0.4.3/dnote_0.4.3_darwin_amd64.tar.gz"
+  version "0.4.3"
+  sha256 "482cb379e938bdc7be23d4a7befcc6ffa8cd72810c03ee32b01e1020ed272f81"
 
   def install
     bin.install "dnote"
