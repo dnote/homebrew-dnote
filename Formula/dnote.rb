f9ce0c937c7d18fa6a29c6504bda61d116579cb7
Sung Won Cho <mikeswcho@gmail.com>
2019-05-18 17:05:45 +1000

    Release 0.7.0

--- Formula/dnote.rb
+++ Formula/dnote.rb
@@ -1,9 +1,9 @@
 class Dnote < Formula
   desc "Capture your learning without leaving the command line"
   homepage "https://dnote.io"
-  url "https://github.com/dnote/cli/releases/download/v0.6.3/dnote_0.6.3_darwin_amd64.tar.gz"
-  version "0.6.3"
-  sha256 "98a3263bb4127eb8c49c36cb81213e10345fc24ecb6ed8599f81d02343b3b467"
+  url "https://github.com/dnote/cli/releases/download/v0.7.0/dnote_0.7.0_darwin_amd64.tar.gz"
+  version "0.7.0"
+  sha256 "55a36a07286c981641c3ff467f1fa1f7e451bce896df5465295cccabcf4cee78"
 
   def install
     bin.install "dnote"
