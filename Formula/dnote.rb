8b3b0bbd24fbc28ad827290ada467b4c6da86d26
Sung Won Cho <sung@monomax.sh>
2019-07-25 17:01:22 +1000

    Add missing shasum

--- Formula/dnote.rb
+++ Formula/dnote.rb
@@ -3,7 +3,7 @@ class Dnote < Formula
   homepage "https://dnote.io"
   url "https://github.com/dnote/dnote/releases/download/cli-v0.8.4/dnote_0.8.4_darwin_amd64.tar.gz"
   version "0.8.4"
-  sha256 ""
+  sha256 "ed1d7b9fc7642c9d2d37049b111ec9a865fede26ff6b8956f362f59b999d48dd"
 
   def install
     bin.install "dnote"
