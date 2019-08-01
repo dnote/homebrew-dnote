da59d76961e2938927b64c8331be92e50b99860a
Sung Won Cho <sung@monomax.sh>
2019-08-01 11:54:46 +1000

    Update dnote.rb

--- Formula/dnote.rb
+++ Formula/dnote.rb
@@ -3,7 +3,7 @@ class Dnote < Formula
   homepage "https://dnote.io"
   url "https://github.com/dnote/dnote/releases/download/cli-v0.9.0/dnote_0.9.0_darwin_amd64.tar.gz"
   version "0.9.0"
-  sha256 ""
+  sha256 "988c503cce0e19bdb4db8ed7c92b4dd182d0af4d26b16761b392e60718b3b687"
 
   def install
     bin.install "dnote"
