57aff8c6c94a17b4bfdbf05868d715a8602b6a80
Sung Won Cho <sung@monomax.sh>
2020-02-06 12:56:38 +1100

    Fix shasum

--- release.sh
+++ release.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # release.sh releases a new version on homebrew using the given version and sha hash
 # use it like this
-# ./release.sh v0.1.0 87b9542629c4944eeb5481022ab6617608d26f16e8f5295b12ee48405f87aca7
+# ./release.sh v0.1.0 path/to/tarball
 
 set -eu
 
 version=$1
-sha=$2
+tarball=$2
+
+sha=$(shasum -a 256 "$tarball" | cut -d ' ' -f 1)
 
 echo "pulling latest dnote-homebrew repo"
 git checkout master
@@ -15,8 +17,8 @@ git pull origin master
 
 cat > ./Formula/dnote.rb << EOF
 class Dnote < Formula
-  desc "Capture your learning without leaving the command line"
-  homepage "https://dnote.io"
+  desc "A simple command line notebook for programmers"
+  homepage "https://www.getdnote.com"
   url "https://github.com/dnote/dnote/releases/download/cli-v${version}/dnote_${version}_darwin_amd64.tar.gz"
   version "${version}"
   sha256 "${sha}"
