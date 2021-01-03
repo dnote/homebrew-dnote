2c3d92c4f61015b7ab7cf2eb6dea9cb4cccfc21d
Sung Won Cho <hello@sungwoncho.com>
2021-01-03 13:49:36 +1100

    Update dnote.rb

--- Formula/dnote.rb
+++ Formula/dnote.rb
@@ -2,7 +2,7 @@ class Dnote < Formula
   desc "Simple command-line notebook for programmers"
   homepage "https://www.getdnote.com"
   url "https://github.com/dnote/dnote/releases/download/cli-v0.12.0/dnote_0.12.0_darwin_amd64.tar.gz"
-  version "0.11.1"
+  version "0.12.0"
   sha256 "4564c10fd4574cd436b84ac56d94935f26ebe7c0ca120d1aaa30cbd8f433f16f"
 
   def install
