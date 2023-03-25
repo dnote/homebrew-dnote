30cafe313e967f0288b96dd9f0e8a695fc4a36f8
Sung <8265228+sungwoncho@users.noreply.github.com>
2023-03-25 17:11:40 +1100

    Create dnote.rb

--- Formula/dnote.rb
+++ Formula/dnote.rb
@@ -3,7 +3,7 @@ class Dnote < Formula
   homepage "https://www.getdnote.com"
   url "https://github.com/dnote/dnote/releases/download/cli-v0.14.0/dnote_0.14.0_darwin_amd64.tar.gz"
   version "0.14.0"
-  sha256 "c34dfd5a7a47e5452a421fe0790d3aa9be5aa1307b95f7e2cff8ca21dee17576 "
+  sha256 "c34dfd5a7a47e5452a421fe0790d3aa9be5aa1307b95f7e2cff8ca21dee17576"
 
   def install
     bin.install "dnote"
